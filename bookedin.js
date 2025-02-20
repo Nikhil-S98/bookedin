dd051b5a3012e00694a850b18a4f9799ea0ecd03
Nikhil-S98 <ns98.nikhil@gmail.com>
2025-02-19 19:07:50 -0500

    first commit

---
+++ bookedin.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const indexRouter = require('./routes/index');
+const authorsRouter = require('./routes/authors');
+
+
+var handlebars = require('express-handlebars').create();
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.use('/', indexRouter);
+app.use('/authors', authorsRouter);
+
+/* GET home page. */
+app.use('/', function(req, res, next) {
+    res.send("<h1>Hello BookedIn</h1>");
+});
+
+app.use('/', indexRouter);
+  
+// custom 404 page
+app.use((req, res) => {
+    res.status(404)
+    res.send('<h1>404 - Not Found</h1>')
+  })
+  
+// custom 500 page
+app.use((err, req, res, next) => {
+  console.error(err.message)
+  res.type('text/plain')
+  res.status(500)
+  res.send('500 - Server Error')
+})
+
+app.listen(port, () => console.log(
+`Express started on http://localhost:${port}; ` +
+`press Ctrl-C to terminate.`))
